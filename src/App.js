9d14b84b39f375686080e79d6c930acb89fad0aa
DeReaper0622 <dereaper@hotmail.com>
2020-07-04 01:42:09 -0400

    adding resources

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Sidebar from './Components/Sidebar/Sidebar';
+// import Sidebar from './Components/sidebar';
+import Introduction from './Components/introduction';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 111
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+         	<Sidebar></Sidebar>
+				<div id="colorlib-main">
+					<Introduction></Introduction>
+          	</div>
+      	</div>
+      </div>
+    );
+  }
 }
 
 export default App;
