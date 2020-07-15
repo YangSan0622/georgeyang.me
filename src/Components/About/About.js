055b2e481dcf04f6c019ab668e5ef9ede46b6dca
DeReaper0622 <dereaper@hotmail.com>
2020-07-14 22:02:17 -0400

    updating experience and about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -13,10 +13,13 @@ class About extends Component {
                                     <div className="col-md-12">
                                         <div className="about-desc">
                                             <span className="heading-meta">About Me</span>
-                                            <h2 className="colorlib-heading">Who Am I?</h2>
-                                            <p>I am an undergraduate student studying Computer Science in University of Waterloo</p>
-                                            <p>I have approximately 2 years of internship/CO-OP experience in Back-End, Front-End, and Video Game development</p>
-                                            <p>I have worked in San Francisco, Santa Clara, Toronto, and Montreal</p>
+                                            <h2 className="colorlib-heading">Something More About Me</h2>
+                                            <p>I am an undergraduate student studying Computer Science in University of Waterloo. Since I am in the COOP program, I have approximately 2 years of internship/CO-OP experience in Back-End, Front-End, and Video Game development</p>
+                                            <p>I have worked in San Francisco, Santa Clara, Toronto, and Montreal. I started doing sports climbing when I was in San Francisco, I mostly do bouldering. 
+                                                I also learned how to do Sprots Fishing when I was there.
+                                            </p>
+                                            <p>I am highly caffeine addicted, during final season I can do two triple shot espresso a day</p>
+                                            <p>I played Saber Fencing </p>
                                         </div>
                                     </div>
                                 </div>
