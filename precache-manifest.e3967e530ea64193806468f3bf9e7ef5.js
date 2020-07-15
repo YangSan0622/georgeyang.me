61b0ed1b70487d0f7999fec9690165bd9bf1eb74
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-15 03:56:22 +0000

    Deploy YangSan0622/georgeyang.me to github.com/YangSan0622/georgeyang.me.git:gh-pages

---
+++ precache-manifest.e3967e530ea64193806468f3bf9e7ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd398aacf0cc0218b786e481dfe572a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f854a5ca9f85830057",
+    "url": "/static/css/main.e547d32c.chunk.css"
+  },
+  {
+    "revision": "f9b75e33025730e95a2e",
+    "url": "/static/js/2.8983405f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8983405f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f854a5ca9f85830057",
+    "url": "/static/js/main.a81db2ec.chunk.js"
+  },
+  {
+    "revision": "eb199739d83dbe6c9709",
+    "url": "/static/js/runtime-main.b71ae6f8.js"
+  }
+]);
